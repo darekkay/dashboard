196a25a1ddf2feeb0e4aa79f1bbf67944cd24ed7
Darek Kay <darekkay@gmail.com>
2019-10-02 14:32:38 +0200

    Register dynamic epics

--- src/state/epics.ts
+++ src/state/epics.ts
@@ -1,6 +1,17 @@
-import { combineEpics } from "redux-observable";
+import { BehaviorSubject } from "rxjs";
+import { mergeMap } from "rxjs/operators";
+import { combineEpics, Epic } from "redux-observable";
 
-import { epic as heartbeatEpic } from "common/ducks/heartbeat";
-import { epic as layoutEpic } from "common/ducks/layout";
+import { epic as heartbeatEpic$ } from "common/ducks/heartbeat";
+import { epic as layoutEpic$ } from "common/ducks/layout";
 
-export const rootEpic = combineEpics(heartbeatEpic, layoutEpic);
+const behaviorSubject = new BehaviorSubject(combineEpics());
+
+export const registerEpic = (epic: Epic) => behaviorSubject.next(epic);
+
+const dynamicEpic$: Epic = (action$, state$) =>
+  behaviorSubject.pipe(
+    mergeMap((epic: Epic) => epic(action$, state$, undefined))
+  );
+
+export const rootEpic = combineEpics(heartbeatEpic$, layoutEpic$, dynamicEpic$);
