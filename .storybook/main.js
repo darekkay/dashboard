d9622bed144a9317af24855e9cfe10da6fc1d1a1
Darek Kay <darekkay@gmail.com>
2020-02-04 20:08:39 +0100

    Update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../src/**/__stories__/*.stories.tsx"],
-  addons: ["@storybook/addon-a11y", "storybook-addon-themes"]
+  addons: [
+    "@storybook/addon-a11y",
+    "storybook-addon-themes",
+    "@storybook/preset-create-react-app"
+  ]
 };
