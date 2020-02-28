dadd35389d36b08c00366eb6d56d82d8c9795247
Darek Kay <darekkay@gmail.com>
2020-02-28 17:54:25 +0100

    Add an option to create a configuration modal within widget generator

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -7,6 +7,15 @@ module.exports = plop => {
     }
   ];
 
+  const widgetPrompts = [
+    ...componentPrompts,
+    {
+      type: "confirm",
+      name: "configurationEnabled",
+      message: "Do you want to use a configuration modal for your widget?"
+    }
+  ];
+
   const componentActions = type => [
     // Component index file
     {
@@ -34,48 +43,62 @@ module.exports = plop => {
     }
   ];
 
-  const widgetActions = [
-    // Widget index file
-    {
-      type: "add",
-      path: "../../src/widgets/{{kebabCase name}}/index.tsx",
-      templateFile: `./widget/index.hbs`,
-      abortOnFail: false
-    },
+  const widgetActions = userInputs => {
+    let widgetActions = [
+      // Widget index file
+      {
+        type: "add",
+        path: "../../src/widgets/{{kebabCase name}}/index.tsx",
+        templateFile: `./widget/index.hbs`,
+        abortOnFail: false
+      },
 
-    // Widget properties
-    {
-      type: "add",
-      path: "../../src/widgets/{{kebabCase name}}/properties.ts",
-      templateFile: "./widget/properties.hbs",
-      abortOnFail: false
-    },
+      // Widget properties
+      {
+        type: "add",
+        path: "../../src/widgets/{{kebabCase name}}/properties.ts",
+        templateFile: "./widget/properties.hbs",
+        abortOnFail: false
+      },
 
-    // Unit tests
-    {
-      type: "add",
-      path: "../../src/widgets/{{kebabCase name}}/__tests__/index.test.tsx",
-      templateFile: "./widget/test.hbs",
-      abortOnFail: false
-    },
+      // Unit tests
+      {
+        type: "add",
+        path: "../../src/widgets/{{kebabCase name}}/__tests__/index.test.tsx",
+        templateFile: "./widget/test.hbs",
+        abortOnFail: false
+      },
 
-    // Widget story
-    {
-      type: "add",
-      path:
-        "../../src/widgets/{{kebabCase name}}/__stories__/index.stories.tsx",
-      templateFile: "./widget/story.hbs",
-      abortOnFail: false
-    },
+      // Widget story
+      {
+        type: "add",
+        path:
+          "../../src/widgets/{{kebabCase name}}/__stories__/index.stories.tsx",
+        templateFile: "./widget/story.hbs",
+        abortOnFail: false
+      },
 
-    // Readme / Documentation
-    {
-      type: "add",
-      path: "../../docs/widgets/{{kebabCase name}}.md",
-      templateFile: "./widget/readme.hbs",
-      abortOnFail: false
+      // Readme / Documentation
+      {
+        type: "add",
+        path: "../../docs/widgets/{{kebabCase name}}.md",
+        templateFile: "./widget/readme.hbs",
+        abortOnFail: false
+      }
+    ];
+
+    if (userInputs.configurationEnabled) {
+      // Widget configuration (if selected)
+      widgetActions.push({
+        type: "add",
+        path: "../../src/widgets/{{kebabCase name}}/configuration.tsx",
+        templateFile: "./widget/configuration.hbs",
+        abortOnFail: false
+      });
     }
-  ];
+
+    return widgetActions;
+  };
 
   plop.setGenerator("Stateless component", {
     description: "Stateless React function",
@@ -91,7 +114,7 @@ module.exports = plop => {
 
   plop.setGenerator("Widget", {
     description: "Dashboard widget",
-    prompts: componentPrompts,
+    prompts: widgetPrompts,
     actions: widgetActions
   });
 };
