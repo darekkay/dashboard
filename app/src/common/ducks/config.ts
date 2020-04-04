6509e70a41fe7d974a34cafa0a395cd33ecb187b
Darek Kay <darekkay@gmail.com>
2020-04-04 10:45:22 +0200

    Use light theme as default for development

--- app/src/common/ducks/config.ts
+++ app/src/common/ducks/config.ts
@@ -6,6 +6,7 @@ import { put, takeEvery } from "redux-saga/effects";
 import { State } from "state/store";
 import { Theme } from "components/settings/theme-select";
 import { importState, handleImportState } from "common/ducks/state";
+import { IS_DEVELOPMENT } from "common/environment";
 
 const SUB_STATE_NAME = "config";
 
@@ -17,10 +18,14 @@ export interface ConfigState {
 const changeTheme = createAction<string>("config/change-theme");
 export const changeLanguage = createAction<string>("config/change-language");
 
-export const defaultTheme = (): Theme =>
-  window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)")
+export const defaultTheme = (): Theme => {
+  if (IS_DEVELOPMENT) {
+    return "default";
+  }
+  return window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)")
     ? "dark"
     : "default";
+};
 
 export const initialState = {
   theme: defaultTheme(),
