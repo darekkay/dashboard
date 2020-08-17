83eb48ec366debaa004b4b5a02d9a329ae50be29
Darek Kay <darekkay@gmail.com>
2020-08-17 23:20:16 +0200

    Prepare server config for API keys

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,5 +1,15 @@
+import logger from "@darekkay/logger";
+
 const config = {
   port: process.env.PORT ?? 3401,
+  api: {},
 };
+
+// output an error if any required API key is missing
+Object.entries(config.api).forEach(([key, value]) => {
+  if (value === undefined) {
+    logger.error(`Missing API key for [${key}]`);
+  }
+});
 
 export default config;
