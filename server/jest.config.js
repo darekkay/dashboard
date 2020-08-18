aee724665230d4d824f2f83c693cff3a87b4b5b0
Darek Kay <darekkay@gmail.com>
2020-08-18 19:45:07 +0200

    Enable Jest coverage for server module

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,8 +1,17 @@
-// TODO: use test coverage
-
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   roots: ["<rootDir>/src"],
+
+  // TODO: improve test coverage
+  coverageThreshold: {
+    global: {
+      statements: 75,
+      branches: 60,
+      functions: 65,
+      lines: 75,
+    },
+  },
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
 };
