bb7d9c3f117217ad3304ac8ff1c700e03d8cd521
Darek Kay <darekkay@gmail.com>
2020-08-18 19:45:07 +0200

    Fix ESLint issue

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,12 +1,10 @@
 import moment from "moment";
 
-import availableWidgets from "../../app/src/widgets/list";
+import { initialMeta } from "../../app/src/widgets/list";
 
 /** Synchronize the TTL (axios cache duration) with the widget's update cycle */
 export const ttlForWidgetType = (widgetType: string) => {
-  const { updateCycle } = (availableWidgets as Record<string, any>)[
-    widgetType
-  ].initialMeta;
+  const { updateCycle } = initialMeta(widgetType);
 
   // The server cache duration is slightly lower than the widget's update cycle
   // to account for the server processing time
