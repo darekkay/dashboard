36549686cbd0c1facc67f77e5df8a03e4e2b69a9
Darek Kay <darekkay@gmail.com>
2020-08-18 19:45:07 +0200

    Update testing-library

--- app/src/components/app/__tests__/index.test.tsx
+++ app/src/components/app/__tests__/index.test.tsx
@@ -8,9 +8,6 @@ describe("<App>", () => {
   test("renders without errors", () => {
     renderConnected(<App />);
     expect(screen.getByRole("main")).toBeInTheDocument();
-
-    // TODO: replace when the issue is fixed
-    // "banner" role is currently broken: https://github.com/testing-library/dom-testing-library/issues/578
-    // expect(screen.getByRole("banner", { hidden: true })).toBeInTheDocument();
+    expect(screen.getByRole("banner", { hidden: true })).toBeInTheDocument();
   });
 });
--- app/src/components/header/__tests__/index.test.tsx
+++ app/src/components/header/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render } from "common/testing";
+import { render, screen } from "common/testing";
 
 import Header from "../index";
 
@@ -15,9 +15,7 @@ describe("<Header />", () => {
       />
     );
 
-    // TODO: replace when the issue is fixed
-    // "banner" role is currently broken: https://github.com/testing-library/dom-testing-library/issues/578
-    // expect(screen.getByRole("banner")).toBeInTheDocument();
+    expect(screen.getByRole("banner")).toBeInTheDocument();
     expect(container.querySelector("header")).toBeInTheDocument();
   });
 });
