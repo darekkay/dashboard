53a8999731559d9dfcd5e735238a8592bf7acca9
Darek Kay <hello@darekkay.com>
2021-05-22 14:56:27 +0200

    Make Dropdown options type generic

--- app/src/components/forms/dropdown/index.tsx
+++ app/src/components/forms/dropdown/index.tsx
@@ -6,7 +6,7 @@ import Label from "components/forms/label";
 
 import "./styles.scss";
 
-const Dropdown: React.FC<Props> = ({
+const Dropdown = <T extends string>({
   className,
   label,
   options,
@@ -14,7 +14,7 @@ const Dropdown: React.FC<Props> = ({
   setValue,
   children,
   ...domProps
-}) => {
+}: Props<T>) => {
   return (
     <Label text={label}>
       <div
@@ -29,7 +29,7 @@ const Dropdown: React.FC<Props> = ({
         <select
           className="w-full pl-4 pr-7 py-1 border rounded bg-color-default text-color-default"
           onChange={(event) => {
-            setValue(event.target.value);
+            setValue(event.target.value as T);
           }}
           {...domProps}
         >
@@ -49,11 +49,12 @@ const Dropdown: React.FC<Props> = ({
   );
 };
 
-export interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
+export interface Props<T extends string>
+  extends React.SelectHTMLAttributes<HTMLSelectElement> {
   label?: string;
-  options: string[];
-  getOptionLabel?: (value: string) => string;
-  setValue: (value: string) => void;
+  options: readonly T[];
+  getOptionLabel?: (value: T) => string;
+  setValue: (value: T) => void;
 }
 
 export default Dropdown;
